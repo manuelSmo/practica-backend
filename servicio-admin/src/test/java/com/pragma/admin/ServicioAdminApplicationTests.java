41020005a6fae3aa0cf11257917cace11b3fed4a
manuelSmo <manuel.smo24@gmail.com>
2021-08-17 18:04:23 -0500

    agregar servicio admin

---
+++ servicio-admin/src/test/java/com/pragma/admin/ServicioAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pragma.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
